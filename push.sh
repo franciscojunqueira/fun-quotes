9b1deeecd51b22d97b1db4546fa9b61b86442159
franciscojunqueira <fjunqueira@gmail.com>
2023-05-22 12:34:41 -0300

    Delete push.sh

--- push.sh
+++
@@ -1,7 +0,0 @@
-
-docker build . -t quotes
-docker login docker-registry-default.cpdh.orizon.local
-docker tag quotes docker-registry-default.cpdh.orizon.local/fature-auditado-dev/quotes:1.0
-docker tag quotes docker-registry-default.cpdh.orizon.local/fature-auditado-dev/quotes:latest
-docker push  docker-registry-default.cpdh.orizon.local/fature-auditado-dev/quotes:1.0
-docker push  docker-registry-default.cpdh.orizon.local/fature-auditado-dev/quotes:latest
